d24c536c648585914cba175f4380c39b2a0b9525
Дима Челомбитько <dima4elom@gmail.com>
2020-10-24 13:26:07 +0300

    Matrix cutter implemented

--- Lab1Filters/Program.cs
+++ Lab1Filters/Program.cs
@@ -9,9 +9,77 @@ namespace Lab1Filters
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            Bitmap img = new Bitmap("imagePath");
+            string path = "image\\Lab1.jpg";
+            ImageReader reader = new ImageReader();
+            Bitmap image = reader.importFromFile(path);
+            MatrixCutter matrixCutter = new MatrixCutter(image);
+
+            Color[][] square = matrixCutter.cutSquareFromImage(0, 0, 9);
+
+            Console.ReadLine();
+
+        }
+
+
+        class ImageReader
+        {
+            public Bitmap importFromFile(string path)
+            {
+                return new Bitmap(path);
+            }
+        }
+
+        class MatrixCutter
+        {
+            private Bitmap image;
+
+            public MatrixCutter(Bitmap image)
+            {
+                this.image = image;
+            }
+
+            //x,y - coordinates of CENTER corner of square
+            public Color[][] cutSquareFromImage(int x, int y, int size)
+            {
+                Color[][] matrix = new Color[size][];
+                for (int i = 0; i < size; i++)
+                {
+                    matrix[i] = new Color[size];
+                }
+
+                int topLeftX = x - size / 2;
+                int topLeftY = y - size / 2;
+
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        int xCord = topLeftX + i;
+                        int yCord = topLeftY + j;
+                        if (Utils.inImage(xCord, yCord, image))
+                        {
+                            matrix[i][j] = image.GetPixel(i, j);
+                        }
+                        else
+                        {
+                            matrix[i][j] = Color.Black;
+                        }
+                    }
+                }
+
+                return matrix;
+
+            }
+        }
+
+        class Utils
+        {
+            public static bool inImage(int indexX, int indexY, Bitmap image)
+            {
+                return indexX > -1 && indexY > -1 && indexX < image.Width && indexY < image.Height;
+            }
         }
     }
 }
