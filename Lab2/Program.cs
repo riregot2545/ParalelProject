f8b58a498bbc43387e24311ae86dfa44867138c2
Дима Челомбитько <dima4elom@gmail.com>
2020-11-08 18:17:59 +0200

    task 7 from lab 2 done

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab2
@@ -10,14 +11,18 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Задача 2 выполняется:");
-            MessageExchange messageExchange = new MessageExchange();
-            MyThread mt1 = new MyThread("Manufacturer", messageExchange, 10);
-            MyThread mt2 = new MyThread("Consumer", messageExchange, 10);
-            mt1.thread.Join();
-            mt2.thread.Join();
+            
+            // Параметры инициализации задачи 7
+            int chickensCount = 5;
+            int foodCount = 30;
+            TimeSpan sleep = TimeSpan.FromMilliseconds(50);
+            TimeSpan chickenExecutionTime = TimeSpan.FromSeconds(5);
 
-            Console.WriteLine("Задача 2 выполнена");
+            Console.WriteLine("Задача 7 выполняется:");
+            FeedingChickens feedingChickens = new FeedingChickens(chickensCount, foodCount, sleep, chickenExecutionTime);
+            feedingChickens.Start();
+            Console.WriteLine("Задача 7 выполнена.");
+
             Console.ReadLine();
         }
     }
